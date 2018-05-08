f53b1171bce24221a8acb22f8e782b8fc51a9037
Radosław <radziowojak@gmail.com>
2018-05-08 22:29:34 +0200

    implemented img download after window is loaded

--- js/imgload.js
+++ js/imgload.js
@@ -1,28 +1,38 @@
+var more540 = window.matchMedia("(min-width: 540px)");
+var more992 = window.matchMedia("(min-width: 992px)");
 function bind_images()
 {
-	var bg = new Image();
-	bg.src = "url(img/slider/max/1.jpg)";
-	document.getElementById('slide1').style.backgroundImage = bg;
+	var size,
+		slide1 = new Image(),
+		slide2 = new Image(),
+		slide3 = new Image()
+		square1 = new Image(),
+		square2 = new Image(),
+		square3 = new Image(),
+		square4 = new Image();
 	
-	var bg = new Image();
-	var bigSrc = 'url(img/slider/max/1.jpg)';
-	var enhancedClass = 'slide-1';
-
-	bg.onload = function () 
-	{
-		document.getElementById('slide1').className += ' ' +enhancedClass;
-	};
-
-	if (bigSrc) {
-		bg.src = bigSrc;
-	}
+	if (more992.matches)
+		size = "max";
+	else if (more540.matches)
+		size = "middle";
+	else
+		size = "min";
 	
-	document.getElementById('slide1').classList.add('slide-1');
-	document.getElementById('slide2').classList.add('slide-2');
-	document.getElementById('slide3').classList.add('slide-3');
+	slide1.src = "img/slider/"+size+"/1.jpg";
+	slide2.src = "img/slider/"+size+"/2.jpg";
+	slide3.src = "img/slider/"+size+"/3.jpg";
 	
-	document.getElementById('square1').classList.add('square-1');
-	document.getElementById('square2').classList.add('square-2');
-	document.getElementById('square3').classList.add('square-3');
-	document.getElementById('square4').classList.add('square-4');
+	square1.src = "img/squares/"+size+"/1.jpg";
+	square2.src = "img/squares/"+size+"/2.jpg";
+	square3.src = "img/squares/"+size+"/3.jpg";
+	square4.src = "img/squares/"+size+"/4.jpg";
+		
+	slide1.onload = function() {document.getElementById('slide1').style.backgroundImage = 'url('+this.src+')';};
+	slide2.onload = function() {document.getElementById('slide2').style.backgroundImage = 'url('+this.src+')';};
+	slide3.onload = function() {document.getElementById('slide3').style.backgroundImage = 'url('+this.src+')';};
+	
+	square1.onload = function() {document.getElementById('square1').style.backgroundImage = 'url('+this.src+')';};
+	square2.onload = function() {document.getElementById('square2').style.backgroundImage = 'url('+this.src+')';};
+	square3.onload = function() {document.getElementById('square3').style.backgroundImage = 'url('+this.src+')';};
+	square4.onload = function() {document.getElementById('square4').style.backgroundImage = 'url('+this.src+')';};
 }
