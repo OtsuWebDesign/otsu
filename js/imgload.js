5153addc3b7ac216a37034e6d01c601f32b85fa7
Radosław <radziowojak@gmail.com>
2018-05-08 14:36:07 +0200

    square images are now dynamically added

--- js/imgload.js
+++ js/imgload.js
@@ -3,4 +3,9 @@ function bind_images()
 	document.getElementById('slide1').classList.add('slide-1');
 	document.getElementById('slide2').classList.add('slide-2');
 	document.getElementById('slide3').classList.add('slide-3');
+	
+	document.getElementById('square1').classList.add('square-1');
+	document.getElementById('square2').classList.add('square-2');
+	document.getElementById('square3').classList.add('square-3');
+	document.getElementById('square4').classList.add('square-4');
 };
