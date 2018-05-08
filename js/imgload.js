5104c9b4369cd7544a27635ca1c59b7a75fa6f93
Radosław <radziowojak@gmail.com>
2018-05-08 17:51:10 +0200

    added pixelated images for slider

--- js/imgload.js
+++ js/imgload.js
@@ -1,5 +1,22 @@
 function bind_images()
 {
+	var bg = new Image();
+	bg.src = "url(img/slider/max/1.jpg)";
+	document.getElementById('slide1').style.backgroundImage = bg;
+	
+	var bg = new Image();
+	var bigSrc = 'url(img/slider/max/1.jpg)';
+	var enhancedClass = 'slide-1';
+
+	bg.onload = function () 
+	{
+		document.getElementById('slide1').className += ' ' +enhancedClass;
+	};
+
+	if (bigSrc) {
+		bg.src = bigSrc;
+	}
+	
 	document.getElementById('slide1').classList.add('slide-1');
 	document.getElementById('slide2').classList.add('slide-2');
 	document.getElementById('slide3').classList.add('slide-3');
@@ -8,4 +25,4 @@ function bind_images()
 	document.getElementById('square2').classList.add('square-2');
 	document.getElementById('square3').classList.add('square-3');
 	document.getElementById('square4').classList.add('square-4');
-};
+}
