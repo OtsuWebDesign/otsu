78c2b4675148ee36e8a0490337337e671ed88e74
Radosław <radziowojak@gmail.com>
2018-05-09 23:10:58 +0200

    full no-jquery hamburger menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,13 @@
-$( document ).ready(function() {
+document.getElementById('hamburger').onclick = function()
+{
+	document.getElementById('menu').classList.add('menu-active');
+	document.getElementById('hamburger').style.display = 'none';
+	document.getElementById('cross').style.display = 'block';
+};
 
-    document.getElementById('hamburger').onclick = function() 
-	{
-		document.getElementById('menu').classList.add('menu-active');
-		document.getElementById('hamburger').style.display = 'none';
-		document.getElementById('cross').style.display = 'block';
-    };
-    
-    $( "#cross" ).click(function() 
-	{
-		document.getElementById('menu').classList.remove('menu-active');
-		document.getElementById('cross').style.display = 'none';
-		document.getElementById('hamburger').style.display = 'block';
-    });
-});
+document.getElementById('cross').onclick = function()
+{
+	document.getElementById('menu').classList.remove('menu-active');
+	document.getElementById('cross').style.display = 'none';
+	document.getElementById('hamburger').style.display = 'block';
+};
