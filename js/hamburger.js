8ff7951dc79d12fe6401c6ea9edd73f499cb0ed2
Radosław <radziowojak@gmail.com>
2018-06-03 22:07:12 +0200

    clippy hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,6 @@
-document.getElementById('hamburger').onclick = function()
+document.getElementById('outerburger').onclick = function()
 {
-	document.getElementById('menu').classList.add('menu-active');
-	document.getElementById('hamburger').style.display = 'none';
-	document.getElementById('cross').style.display = 'block';
-};
-
-document.getElementById('cross').onclick = function()
-{
-	document.getElementById('menu').classList.remove('menu-active');
-	document.getElementById('cross').style.display = 'none';
-	document.getElementById('hamburger').style.display = 'block';
+	document.getElementById('menu').classList.toggle('menu-active');
+	document.getElementById('hamburger').classList.toggle('icon-cross');
+	document.getElementById('hamburger').classList.toggle('icon-hamburger');
 };
