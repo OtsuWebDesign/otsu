38c755f93aa4ddb9b55716c8daaaebbda1c5dfb1
Radosław <radziowojak@gmail.com>
2018-06-19 14:06:16 +0200

    added aos to elements in index and minor changes

--- js/more.js
+++ js/more.js
@@ -3,22 +3,16 @@ function hidemore()
 	var showmores = document.getElementsByClassName('showmore');
 	for (let i = 0; i < showmores.length; i++) 
 	{
-		showmores[i].classList.toggle('nopacity');
+		showmores[i].classList.toggle('novisibility');
 	}
 	var morecontents = document.getElementsByClassName('morecontent');
 	for (let i = 0; i < morecontents.length; i++) 
 	{
 		morecontents[i].classList.toggle('less');
 	}
-	var showlesss = document.getElementsByClassName('showlesss');
-	for (let i = 0; i < showlesss.length; i++) 
-	{
-		showlesss[i].classList.toggle('nopacity');
-	}
 }
 function togglemore(topic_id)
 {
-	document.querySelector("[showmore-id='"+topic_id+"']").classList.toggle('nopacity');
+	document.querySelector("[showmore-id='"+topic_id+"']").classList.toggle('novisibility');
 	document.querySelector("[topic-id='"+topic_id+"']").classList.toggle('less');
-	document.querySelector("[showless-id='"+topic_id+"']").classList.toggle('nopacity');
 };
