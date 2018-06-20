5dabb0e265c1141654a988ebda050ef629a6724a
Radosław <radziowojak@gmail.com>
2018-06-20 23:13:17 +0200

    fixed scroll blocking on large screens (showless issue)

--- js/scrollto.js
+++ js/scrollto.js
@@ -7,49 +7,23 @@ document.querySelector('[showless-id="2"]').addEventListener('click', () => {
 document.querySelector('[showless-id="2"]').addEventListener('click', () => {
 	scrollIt(document.querySelector('#topic2'));
 });
-function scrollIt(destination, owerscroll = 300, duration = 1000, easing = 'easeOutQuad', callback) {
+function scrollIt(destination, owerscroll = 100, duration = 1000, easing = 'easeOutQuad', callback) {
 
-  const easings = {
-    linear(t) {
-      return t;
-    },
-    easeInQuad(t) {
-      return t * t;
-    },
-    easeOutQuad(t) {
-      return t * (2 - t);
-    },
-    easeInOutQuad(t) {
-      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-    },
-    easeInCubic(t) {
-      return t * t * t;
-    },
-    easeOutCubic(t) {
-      return (--t) * t * t + 1;
-    },
-    easeInOutCubic(t) {
-      return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-    },
-    easeInQuart(t) {
-      return t * t * t * t;
-    },
-    easeOutQuart(t) {
-      return 1 - (--t) * t * t * t;
-    },
-    easeInOutQuart(t) {
-      return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
-    },
-    easeInQuint(t) {
-      return t * t * t * t * t;
-    },
-    easeOutQuint(t) {
-      return 1 + (--t) * t * t * t * t;
-    },
-    easeInOutQuint(t) {
-      return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-    }
-  };
+	const easings = {
+		linear(t) {return t;},
+		easeInQuad(t) {return t * t;},
+		easeOutQuad(t) {return t * (2 - t);},
+		easeInOutQuad(t) {return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;},
+		easeInCubic(t) {return t * t * t;},
+		easeOutCubic(t) {return (--t) * t * t + 1;},
+		easeInOutCubic(t) {return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;},
+		easeInQuart(t) {return t * t * t * t;},
+		easeOutQuart(t) {return 1 - (--t) * t * t * t;},
+		easeInOutQuart(t) {return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;},
+		easeInQuint(t) {return t * t * t * t * t;},
+		easeOutQuint(t) {return 1 + (--t) * t * t * t * t;},
+		easeInOutQuint(t) {return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;}
+	};
 
   const start = window.pageYOffset;
   const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
@@ -71,8 +45,10 @@ function scrollIt(destination, owerscroll = 300, duration = 1000, easing = 'ease
     const now = 'now' in window.performance ? performance.now() : new Date().getTime();
     const time = Math.min(1, ((now - startTime) / duration));
     const timeFunction = easings[easing](time);
-    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
-
+	if(documentHeight - start > windowHeight)
+		window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+	else
+		break;
     if (window.pageYOffset === destinationOffsetToScroll) {
       if (callback) {
         callback();
