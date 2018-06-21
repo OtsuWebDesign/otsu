c07feda778b79b0bb6e074c78c5fa4381de65924
Radosław <radziowojak@gmail.com>
2018-06-21 13:25:58 +0200

    scroll js optimalization

--- js/scrollto.js
+++ js/scrollto.js
@@ -4,36 +4,14 @@ document.querySelector('[showless-id="1"]').addEventListener('click', () => {
 document.querySelector('[showless-id="2"]').addEventListener('click', () => {
 	scrollIt(document.querySelector('.morecontent[topic-id="2"]'));
 });
-function scrollIt(destination, overscroll = 100, duration = 800, easing = 'linear', callback) {
-
-	const easings = {
-		linear(t) {return t;},
-		easeInQuad(t) {return t * t;},
-		easeOutQuad(t) {return t * (2 - t);},
-		easeInOutQuad(t) {return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;},
-		easeInCubic(t) {return t * t * t;},
-		easeOutCubic(t) {return (--t) * t * t + 1;},
-		easeInOutCubic(t) {return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;},
-		easeInQuart(t) {return t * t * t * t;},
-		easeOutQuart(t) {return 1 - (--t) * t * t * t;},
-		easeInOutQuart(t) {return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;},
-		easeInQuint(t) {return t * t * t * t * t;},
-		easeOutQuint(t) {return 1 + (--t) * t * t * t * t;},
-		easeInOutQuint(t) {return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;}
-	};
+function scrollIt(destination, duration = 800) {
 
 	const start = window.pageYOffset;
 	const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-
-	const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-	const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-	const destinationHeight = destination.offsetHeight;
-	const destinationOffsetToScroll =  start - destinationHeight;
+	const destinationOffsetToScroll =  start - destination.offsetHeight;
+	
 	if ('requestAnimationFrame' in window === false) {
 		window.scroll(0, destinationOffsetToScroll);
-		if (callback) {
-			callback();
-		}
 		return;
 	}
 	
@@ -41,17 +19,12 @@ function scrollIt(destination, overscroll = 100, duration = 800, easing = 'linea
 	{
 		const now = 'now' in window.performance ? performance.now() : new Date().getTime();
 		const time = Math.min(1, ((now - startTime) / duration));
-		const timeFunction = easings[easing](time);
 
-		window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+		window.scroll(0, Math.ceil((time * (destinationOffsetToScroll - start)) + start));
 		if (window.pageYOffset === destinationOffsetToScroll) {
-			if (callback) {
-			callback();
-			}
 			return;
 		}
-		requestAnimationFrame(scroll);
+		requestAnimationFrame(scroll, 0.1);
 	}
-	
 	scroll();
 }
