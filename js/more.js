f5f287939dcfb6994549dad5d07bb819a076fe18
Radosław <radziowojak@gmail.com>
2018-06-21 12:53:35 +0200

    renoved useless code

--- js/more.js
+++ js/more.js
@@ -1,16 +1,3 @@
-function hidemore()
-{
-	var showmores = document.getElementsByClassName('showmore');
-	for (let i = 0; i < showmores.length; i++) 
-	{
-		showmores[i].classList.toggle('novisibility');
-	}
-	var morecontents = document.getElementsByClassName('morecontent');
-	for (let i = 0; i < morecontents.length; i++) 
-	{
-		morecontents[i].classList.toggle('less');
-	}
-}
 function togglemore(topic_id)
 {
 	document.querySelector("[showmore-id='"+topic_id+"']").classList.toggle('novisibility');
