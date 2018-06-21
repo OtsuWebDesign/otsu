27d6e25a10590ba0133d2da8c452ee3b42a89ff4
Radosław <radziowojak@gmail.com>
2018-06-21 22:53:50 +0200

    max height of morecontent in set by js

--- js/more.js
+++ js/more.js
@@ -1,5 +1,20 @@
 function togglemore(topic_id)
 {
 	document.querySelector("[showmore-id='"+topic_id+"']").classList.toggle('novisibility');
-	document.querySelector("[topic-id='"+topic_id+"']").classList.toggle('less');
+	if(document.querySelector("[topic-id='"+topic_id+"']").classList.contains('less'))
+	{
+		document.querySelector("[topic-id='"+topic_id+"']").classList.remove('less');
+		setTimeout(function()
+		{
+			document.querySelector("[topic-id='"+topic_id+"']").style.maxHeight = document.querySelector("[topic-id='"+topic_id+"']").offsetHeight+'px';
+		}, 900);
+	}
+	else 
+	{
+		document.querySelector("[topic-id='"+topic_id+"']").classList.add('less');
+		setTimeout(function()
+		{
+			document.querySelector("[topic-id='"+topic_id+"']").removeAttribute('style');
+		}, 900);
+	}
 };
