7d8704abd714ff15658be2706756732d1dafffd0
Radosław <radziowojak@gmail.com>
2018-06-26 17:47:45 +0200

    renamed navcolor.js to nav.js and moved there hamburger.js

--- js/hamburger.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('outerburger').onclick = function()
-{
-	document.getElementById('menu').classList.toggle('menu-active');
-	document.getElementById('hamburger').classList.toggle('menu');
-	document.getElementById('hamburger').classList.toggle('close');
-};
