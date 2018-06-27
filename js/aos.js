9f526b471cbb56aef8a3a5090491872955d38668
Radosław <radziowojak@gmail.com>
2018-06-28 00:09:01 +0200

    added primitive aos js

--- js/aos.js
+++ js/aos.js
@@ -1,4 +1,14 @@
 function aos()
 {
-	var elements = document.getElementsByClassName('aos')
+	const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+	const scrolled = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+	const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+	var elements = document.getElementsByClassName('aos');
+	for(let i=0; i< elements.length; i++)
+	{
+		const elementOffset = elements[i].offsetTop;
+		const elementHeight = elements[i].offsetHeight;
+		if(scrolled + windowHeight > elementOffset + elementHeight)
+			elements[i].classList.add('aos-animated');
+	}
 }
