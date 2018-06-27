951e3e79df08e5d0259b070ac6f6258359eab6b2
Radosław <radziowojak@gmail.com>
2018-06-28 01:07:35 +0200

    changed element offset to working one

--- js/aos.js
+++ js/aos.js
@@ -1,13 +1,13 @@
 function aos()
 {
 	const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-	const scrolled = window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+	var scrolled = Math.max(window.pageYOffset, document.body.scrollTop, document.documentElement.scrollTop);
 	const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
 	var elements = document.getElementsByClassName('aos');
 	for(let i=0; i< elements.length; i++)
 	{
-		const elementOffset = elements[i].offsetTop;
-		const elementHeight = elements[i].offsetHeight;
+		var elementOffset = elements[i].getBoundingClientRect().top + window.scrollY;;
+		var elementHeight = elements[i].offsetHeight;
 		if(scrolled + windowHeight > elementOffset + elementHeight)
 			elements[i].classList.add('aos-animate');
 		else
