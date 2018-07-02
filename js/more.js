bbd7dc88fa341bfa52c741ccdd4ba8ec1c6cea3e
Radosław <radziowojak@gmail.com>
2018-07-02 23:39:47 +0200

    when we know how tall is content in showmore, we do not delete it - improved showmore animation

--- js/more.js
+++ js/more.js
@@ -15,9 +15,5 @@ function togglemore(topic_id)
 		document.getElementsByClassName("showless-"+topic_id)[0].classList.add('novisibility');
 		setTimeout(() => document.getElementsByClassName("showmore-"+topic_id)[0].classList.remove('novisibility') ,500);
 		document.getElementsByClassName("morecontent-"+topic_id)[0].classList.add('less');
-		setTimeout(function()
-		{
-			document.getElementsByClassName("morecontent-"+topic_id)[0].removeAttribute('style');
-		}, 1050);
 	}
 };
