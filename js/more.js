fdf98ae41daff11235dc212a1b7119f22248b90a
Radosław <radziowojak@gmail.com>
2018-07-07 01:05:38 +0200

    changed showmore to better one

--- js/more.js
+++ js/more.js
@@ -1,19 +1,21 @@
 function togglemore(topic_id)
 {
-	if(document.getElementsByClassName("morecontent-"+topic_id)[0].classList.contains('less'))
-	{
-		document.getElementsByClassName("showmore-"+topic_id)[0].classList.add('novisibility');
-		setTimeout(() => document.getElementsByClassName("showless-"+topic_id)[0].classList.remove('novisibility') ,500);
-		document.getElementsByClassName("morecontent-"+topic_id)[0].classList.remove('less');
-		setTimeout(function()
-		{
-			document.getElementsByClassName("morecontent-"+topic_id)[0].style.maxHeight = document.getElementsByClassName("morecontent-"+topic_id)[0].offsetHeight+'px';
-		}, 1050);
-	}
-	else 
+	let morecontent = document.getElementsByClassName("morecontent-"+topic_id)[0];
+	if(morecontent.style.maxHeight)
 	{
 		document.getElementsByClassName("showless-"+topic_id)[0].classList.add('novisibility');
 		setTimeout(() => document.getElementsByClassName("showmore-"+topic_id)[0].classList.remove('novisibility') ,500);
-		document.getElementsByClassName("morecontent-"+topic_id)[0].classList.add('less');
+		morecontent.style.maxHeight = null;
+		morecontent.style.margin = null;
+	}
+	else 
+	{
+		document.getElementsByClassName("showmore-"+topic_id)[0].classList.add('novisibility');
+		setTimeout(() => document.getElementsByClassName("showless-"+topic_id)[0].classList.remove('novisibility') ,500);
+		morecontent.style.maxHeight = morecontent.scrollHeight+'px';
+		if(morecontent.classList.contains('morecontent-1'))
+			morecontent.style.marginTop = 'calc(-10px - 5.1vw)';
+		else
+			morecontent.style.marginTop = 'calc(-20px - 3vw)';
 	}
 };
