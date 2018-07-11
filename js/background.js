1eec4518f8a152d7b169c6fcd79eb01a6ac0346e
Radosław <radziowojak@gmail.com>
2018-07-11 11:32:02 +0200

    every element has different changedirection time and direction, supporting many containers

--- js/background.js
+++ js/background.js
@@ -2,47 +2,53 @@ const colors = ["red"];
 const numBalls = 10;
 
 var balls = [];
-const container = document.getElementById('container');
-for (let i = 0; i < numBalls; i++) {
-	let ball = document.createElement("div");
-	ball.classList.add("ball");
-	ball.style.borderColor = colors[Math.floor(Math.random() * colors.length)];
-	ball.style.left = `${Math.floor(Math.random() * (container.clientWidth-50))}px`;
-	ball.style.top = `${Math.floor(Math.random() * (container.clientHeight-50))}px`;
-	//ball.style.transform = `scale(${Math.random()})`;
-	ball.style.width = `${Math.random()*100+100}px`;
-	ball.style.height = ball.style.width;
+var containers = [document.getElementById('container')];
 
-	balls.push(ball);
-	container.append(ball);
-}
-
-
-balls.forEach((el) => {
-	setInterval(frame, 30);
-	setInterval(changeDirection, 500, Math.random() > 0.5 ? 1 : 0);
-	let directionX = directionY = (Math.random() > 0.5 ? 1 : -1);
-	function frame()
-	{
-		let toX = Math.floor(el.offsetLeft + directionX * Math.random() + directionX); 
-		let toY = Math.floor(el.offsetTop + directionY * Math.random() + directionY); 
-		if(toX > container.clientWidth-50 || toX < 50)
-		{
-			directionX == 1 ? directionX = -1 : directionX = 1;
-			toX = (toX > container.clientWidth-50 ? container.clientWidth-50 : 50);
-		}
-		if(toY > container.clientHeight-50 || toY < 50)
-		{
-			directionY == 1 ? directionY = -1 : directionY = 1;
-			toY = (toY > container.clientHeight-50 ? container.clientHeight-50 : 50); 
-		}
-		el.style.left = toX + 'px'; 
-		el.style.top = toY + 'px';
+containers.forEach((container) =>
+{
+	for (let i = 0; i < numBalls; i++) {
+		let ball = document.createElement("div");
+		ball.classList.add("ball");
+		ball.style.borderColor = colors[Math.floor(Math.random() * colors.length)];
+		ball.style.left = `${Math.floor(Math.random() * (container.clientWidth-50))}px`;
+		ball.style.top = `${Math.floor(Math.random() * (container.clientHeight-50))}px`;
+		//ball.style.transform = `scale(${Math.random()})`;
+		ball.style.width = `${Math.random()*100+100}px`;
+		ball.style.height = ball.style.width;
+		ball.dirx = (Math.random() > 0.5 ? 1 : -1);
+		ball.diry = (Math.random() > 0.5 ? 1 : -1);
+		ball.changeTime = Math.random()*300 + 600;
+		balls.push(ball);
+		container.append(ball);
 	}
-	function changeDirection(xy)
-	{
-		xy == 0 ?
-		directionX = Math.random() > 0.5 ? 1 : -1 :
-		directionY = Math.random() > 0.5 ? 1 : -1;
-	}
+
+
+	balls.forEach((el) => {
+		setInterval(frame, 30);
+		setInterval(changeDirection, el.changeTime, el, Math.floor(Math.random() < 0.5 ? 0 : 1));
+		function frame()
+		{
+			let toX = Math.floor(el.offsetLeft + el.dirx * Math.random() + el.dirx); 
+			let toY = Math.floor(el.offsetTop + el.diry * Math.random() + el.diry); 
+			if(toX > container.clientWidth-50 || toX < 50)
+			{
+				el.dirx == 1 ? el.dirx = -1 : el.dirx = 1;
+				toX = (toX > container.clientWidth-50 ? container.clientWidth-50 : 50);
+			}
+			if(toY > container.clientHeight-50 || toY < 50)
+			{
+				el.diry == 1 ? el.diry = -1 : el.diry = 1;
+				toY = (toY > container.clientHeight-50 ? container.clientHeight-50 : 50); 
+			}
+			el.style.left = toX + 'px'; 
+			el.style.top = toY + 'px';
+		}
+	});
+
 });
+function changeDirection(element, xy)
+{
+	xy == 0 ?
+	element.dirx = Math.random() > 0.5 ? 1 : -1 :
+	element.diry = Math.random() > 0.5 ? 1 : -1;
+}
