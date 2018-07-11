76cebbcee685d19dfbd665c5f6307875a6a6faad
Radosław <radziowojak@gmail.com>
2018-07-11 13:25:45 +0200

    added comments

--- js/background.js
+++ js/background.js
@@ -14,32 +14,32 @@ function Square(cont)
 	div.style.width = `${Math.random()*100+100}px`;
 	div.style.height = div.style.width;
 
-	this.dirx = (Math.random() > 0.5 ? 1 : -1);
+	this.dirx = (Math.random() > 0.5 ? 1 : -1);//choose (randomly) initial direction
 	this.diry = (Math.random() > 0.5 ? 1 : -1);
-	this.changeTime = Math.random()*300 + 600;
+	this.changeTime = Math.random()*300 + 600;//randomly assign changePosition interval
 	cont.append(div);
 	this.frame = function()
 	{
-		square.toX = Math.floor(div.offsetLeft + square.dirx); 
+		square.toX = Math.floor(div.offsetLeft + square.dirx); //set where to move (where it is + 1px in chosen direction)
 		square.toY = Math.floor(div.offsetTop + square.diry); 
-		if(square.toX > cont.clientWidth-50 || square.toX < 50)
+		if(square.toX > cont.clientWidth-50 || square.toX < 50) //check if not running out of container in x
 		{
-			square.dirx == 1 ? square.dirx = -1 : square.dirx = 1;
-			square.toX = (square.toX > cont.clientWidth-50 ? cont.clientWidth-50 : 50);
+			square.dirx == 1 ? square.dirx = -1 : square.dirx = 1; //if yes, change direction
+			square.toX = (square.toX > cont.clientWidth-50 ? cont.clientWidth-50 : 50); //change move to new direction
 		}
-		if(square.toY > cont.clientHeight-50 || square.toY < 50)
+		if(square.toY > cont.clientHeight-50 || square.toY < 50)//check if not running out of container in y
 		{
 			square.diry == 1 ? square.diry = -1 : square.diry = 1;
 			square.toY = (square.toY > cont.clientHeight-50 ? cont.clientHeight-50 : 50); 
 		}
-		div.style.left = square.toX + 'px'; 
+		div.style.left = square.toX + 'px'; //move element
 		div.style.top = square.toY + 'px';
 	}
 	this.changeDirection = function()
 	{
-		Math.random() < 0.5 ? 
-		(square.dirx = Math.random() > 0.5 ? 1 : -1) : 
-		(square.diry = Math.random() > 0.5 ? 1 : -1);
+		Math.random() < 0.5 ? //choose (randomly) which direction change (x or y)
+		(square.dirx = Math.random() > 0.5 ? 1 : -1) :	//choose (randomly) direction (right or left)
+		(square.diry = Math.random() > 0.5 ? 1 : -1);	//(down or up)
 	}
 }
 
