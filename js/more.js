efba13a9ddba4c508d93785b770aa198be035d5a
Radosław <radziowojak@gmail.com>
2018-07-24 14:32:40 +0200

    zmiany w indexie - dodano filar o PWA

--- js/more.js
+++ js/more.js
@@ -5,10 +5,7 @@ function showmore(topic_id)
 	document.getElementsByClassName("showmore-"+topic_id)[0].classList.add('novisibility');
 	setTimeout(() => document.getElementsByClassName("showless-"+topic_id)[0].classList.remove('novisibility') ,500);
 	morecontent.style.maxHeight = morecontent.scrollHeight+'px';
-	if(morecontent.classList.contains('morecontent-1'))
-		morecontent.style.marginTop = 'calc(-10px - 5.1vw)';
-	else
-		morecontent.style.marginTop = 'calc(-20px - 3vw)';
+	morecontent.style.marginTop = 'calc(-15px - 2vw)';
 };
 function hidemore(topic_id)
 {
