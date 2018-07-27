f37ea10813ceb8f4416e217a7bab52dfbdeda623
Maciej Witkowski <d3lusion@post.cz>
2018-07-27 21:24:00 +0200

    Changed sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var swVersion = 'v1';
-var CACHE_FILES = 'otsu.pl-files-cache-v6';
-var CACHE_IMAGES = 'otsu.pl-images-cache-v6';
-var CACHE_OTHER = 'otsu.pl-other-cache-v4';
+var CACHE_FILES = 'otsu.pl-files-cache-v7';
+var CACHE_IMAGES = 'otsu.pl-images-cache-v7';
+var CACHE_OTHER = 'otsu.pl-other-cache-v5';
 var cache_whitelist = [CACHE_FILES, CACHE_IMAGES, CACHE_OTHER];
 var urlsToCache_FILES = [
 	'/',
