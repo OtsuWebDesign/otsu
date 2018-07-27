44a3b9e759feec340720f890c834274567dd6e5e
Maciej Witkowski <d3lusion@post.cz>
2018-07-27 20:24:18 +0200

    did nothing, but git wants me to add a commit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 var swVersion = 'v1';
-var CACHE_FILES = 'otsu.pl-files-cache-v4';
-var CACHE_IMAGES = 'otsu.pl-images-cache-v4';
-var CACHE_OTHER = 'otsu.pl-other-cache-v2';
+var CACHE_FILES = 'otsu.pl-files-cache-v5';
+var CACHE_IMAGES = 'otsu.pl-images-cache-v5';
+var CACHE_OTHER = 'otsu.pl-other-cache-v3';
 var cache_whitelist = [CACHE_FILES, CACHE_IMAGES, CACHE_OTHER];
 var urlsToCache_FILES = [
 	'/',
