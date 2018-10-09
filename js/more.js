de835193f9e2138ad431d7346181f8752a1beba4
Radosław <radziowojak@gmail.com>
2018-10-09 09:07:41 +0200

    przywrucono nieskompilowany js i html

--- js/more.js
+++ js/more.js
@@ -1,3 +1,17 @@
-"use strict";function showmore(topic_id)
-{let morecontent=document.getElementsByClassName("morecontent-"+topic_id)[0];document.getElementsByClassName("showmore-"+topic_id)[0].classList.add('novisibility');setTimeout(()=>document.getElementsByClassName("showless-"+topic_id)[0].classList.remove('novisibility'),500);morecontent.style.maxHeight=morecontent.scrollHeight+'px';morecontent.style.marginTop='calc(-15px - 2vw)'};function hidemore(topic_id)
-{let morecontent=document.getElementsByClassName("morecontent-"+topic_id)[0];document.getElementsByClassName("showless-"+topic_id)[0].classList.add('novisibility');setTimeout(()=>document.getElementsByClassName("showmore-"+topic_id)[0].classList.remove('novisibility'),500);morecontent.style.maxHeight=null;morecontent.style.margin=null}
+"use strict";//do poprawnego działania zmiennych o zasięgy blokowym (np. let)
+function showmore(topic_id)
+{
+	let morecontent = document.getElementsByClassName("morecontent-"+topic_id)[0];
+	document.getElementsByClassName("showmore-"+topic_id)[0].classList.add('novisibility');
+	setTimeout(() => document.getElementsByClassName("showless-"+topic_id)[0].classList.remove('novisibility') ,500);
+	morecontent.style.maxHeight = morecontent.scrollHeight+'px';
+	morecontent.style.marginTop = 'calc(-15px - 2vw)';
+};
+function hidemore(topic_id)
+{
+	let morecontent = document.getElementsByClassName("morecontent-"+topic_id)[0];
+	document.getElementsByClassName("showless-"+topic_id)[0].classList.add('novisibility');
+	setTimeout(() => document.getElementsByClassName("showmore-"+topic_id)[0].classList.remove('novisibility') ,500);
+	morecontent.style.maxHeight = null;
+	morecontent.style.margin = null;
+};
